78b040c32b15d3ef83be0071291f1730fabbff78
Francisco Alessandro Clemente Silva <alessandrosilvastacasa@gmail.com>
2023-04-11 07:32:30 -0300

    Ajustes3

--- src/Pages/DeatilPage/DetailPage.jsx
+++ src/Pages/DeatilPage/DetailPage.jsx
@@ -15,27 +15,27 @@ const DetailPage = () => {
     const {cd_con_pag,cd_pagcon_pag} = useParams();
     let navigate = useNavigate();
 
-       useEffect (() => {
-        async function loadDatas(){
-          const responsePagCon = await getContasPagasID(cd_pagcon_pag);
-            setDetails(responsePagCon);
-        //   const responseDetails =  await getDetailsContas(cd_con_pag)
-        //     setDatas(responseDetails);
-        } loadDatas();
-    },[]);
+    //    useEffect (() => {
+    //     async function loadDatas(){
+    //       const responsePagCon = await getContasPagasID(cd_pagcon_pag);
+    //         setDetails(responsePagCon);
+    //     //   const responseDetails =  await getDetailsContas(cd_con_pag)
+    //     //     setDatas(responseDetails);
+    //     } loadDatas();
+    // },[]);
 
-    const returnAllDetails = (val , index) =>{
-        return(
-        <ul key={index}>
-            {/* <li><label>N° Fornecedor: </label> {val['NR_DOCUMENTO']} </li> */}
-            <li><label>Nome: </label> {val['NM_FORNECEDOR']}</li>
-            <li><label>Descrição: </label> {val['DESCRICAO']}    </li>
-            <li><label>Data Pagamento:</label> {val['DT_PAGAMENTO']} </li>
-            <li><label>Parcela:</label> {val['PARCELA']}      </li>
-            <li><label>Valor Pago:</label> R$ {val['VL_PAGO']}   </li>
-        </ul>
-        )
-      }
+    // const returnAllDetails = (val , index) =>{
+    //     return(
+    //     <ul key={index}>
+    //         {/* <li><label>N° Fornecedor: </label> {val['NR_DOCUMENTO']} </li> */}
+    //         <li><label>Nome: </label> {val['NM_FORNECEDOR']}</li>
+    //         <li><label>Descrição: </label> {val['DESCRICAO']}    </li>
+    //         <li><label>Data Pagamento:</label> {val['DT_PAGAMENTO']} </li>
+    //         <li><label>Parcela:</label> {val['PARCELA']}      </li>
+    //         <li><label>Valor Pago:</label> R$ {val['VL_PAGO']}   </li>
+    //     </ul>
+    //     )
+    //   }
 
     return (
       
